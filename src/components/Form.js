0521706296e1f0dcd4762432cc4a0350e7141079
Malvin Opondo <malvinopondo@gmail.com>
2023-09-14 22:36:29 -0700

    feat(Form.js): Create two columns

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,12 @@ function Form() {
   return (
     <section>
         <div className='register'>
-            <div className='col-1'></div>
+            <div className='col-1'>
+                <h2>Sign In</h2>
+                <span>register and enjoy the service</span>
+
+                
+            </div>
             <div className='col-2'></div>
         </div>
     </section>
