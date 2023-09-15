04ebf2989319c58675595527a205be44f891286e
Malvin Opondo <malvinopondo@gmail.com>
2023-09-15 08:20:59 -0700

    fix(Debug): Debugging of the Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,44 @@
-import React from 'react'
-import bgimg from "../assets/bgimg.png";
+import React from "react";
+// import bgimg from "../assets/bgimg.png";
+import logo from "../assets/logo.png";
 
 function Form() {
-    return (
-        <section>
-            <div className='register'>
-                <div className='col-1'>
+  return (
+    <section>
+      <div className="register">
+        <div className="col-1">
+          <img src={logo} alt="" id="logo" width='200px' height='50px' />
 
-                    <form id='form' className='flex flex-col'>
-                        <span><h1 className='custom'>Create a Custom Gift Card</h1></span>
+          <form id="form" className="flex flex-col">
+            <span>
+              <h1 className="custom">Create a Custom Gift Card</h1>
+            </span>
+            <span>
+              <h2>To</h2>
+            </span>
+            <input type="text" placeholder="Recipient Name" />
+            <span>
+              <h2>From</h2>
+            </span>
+            <input type="text" placeholder="Your Name" />
+            <span>
+              <h2 >Amount(USD)</h2>
+            </span>
+            <input type="text" placeholder="$" />
+            <span>
+              <h2>Your message</h2>
+            </span>
+            <input type="text" placeholder="" id="message" />
+          </form>
+          <span>
+            <button id="btn">Generate</button>
+          </span>
+        </div>
 
-                        <span>
-                            <h2>To</h2>
-                        </span>
-                        <input type='text' placeholder='Recipient Name' />
-                        <span>
-                            <h2>From</h2>
-                        </span>
-                        <input type='text' placeholder='Your Name' />
-                        <span>
-                            <h2>Amount(USD)</h2>
-                        </span>
-                        <input type='text' placeholder='$' />
-                        <span>
-                            <h2>Your message</h2>
-                        </span>
-                        < input type='text' placeholder='' id='message' />
-
-                    </form>
-                    <span>
-                        <button id="btn">Generate</button></span>
-                </div>
-
-                <div className='col-2'>
-                    <img src={bgimg} alt=''></img>
-                </div>
-            </div>
-        </section>
-    )
+        <div className="col-2">{/* <img src={bgimg} alt='' id='img'/> */}</div>
+      </div>
+    </section>
+  );
 }
 
-export default Form
+export default Form;
