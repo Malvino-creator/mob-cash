99328489e5cb2459014979e5d41ec0d63e4537fb
Malvin Opondo <malvinopondo@gmail.com>
2023-09-15 13:52:48 -0700

    fix(Debug): Debug the app

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,14 +7,13 @@ function Form() {
     <section>
       <div className="register">
         <div className="col-1">
-          <img src={logo} alt="" id="logo" width="250px" height="80px" />
+          <img src={logo} alt="" id="logo" width="230px" height="70px" />
 
           <form id="form" className="flex flex-col">
-            <span>
-              <h1 className="custom">
+              <p className="custom">
                 <strong>Create a Custom Gift Card</strong>
-              </h1>
-            </span>
+              </p>
+            
             <span>
               <h2>To</h2>
             </span>
@@ -40,17 +39,18 @@ function Form() {
         <div className="col-2">
           <img src={bgimg} alt="" id="img" />
 
-          <button className="proceed-btn">Click To Proceed</button>
+          <button className="proceed-btn">Proceed to Buy</button>
           <p id="terms">
             By clicking proceed you agree to our{" "}
             <strong>
-              <a href="privacy">Gift Cards Terms of Use</a>
+              <a href="privacy"><br></br>Gift Cards Terms of Use</a>
             </strong>{" "}
             and{" "}
             <strong>
               <a href="privacy">Privacy Policy</a>
             </strong>
           </p>
+          
           <span id="copyright"> &copy; Copyright 2023 The MobiShare Corporation</span>
         </div>
       </div>
