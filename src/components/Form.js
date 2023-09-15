e410747eb94b665cef48da5c0c529f7ed000c009
Malvin Opondo <malvinopondo@gmail.com>
2023-09-15 00:39:57 -0700

    fix(App.css): Editing styling of the app

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,32 @@
 import React from 'react'
-import bgimg from "./assets/img1";
+import bgimg from "../assets/bgimg.png";
 
 function Form() {
-  return (
-    <section>
-        <div className='register'>
-            <div className='col-1'>
-                <h2>Sign In</h2>
-                <span>register and enjoy the service</span>
+    return (
+        <section>
+            <div className='register'>
+                <div className='col-1'>
+
+                    <span>Create a Custom Gift Card</span>
 
 
-                <form id='form' className='flex flex-col'>
-                    <input type='text' placeholder='Recipient Name'/>
-                    <input type='text' placeholder='Your Name'/>
-                    <input type='text' placeholder='$'/>
-                    <input type='text' placeholder='Your message'/>
+                    <form id='form' className='flex flex-col'>
+                        <input type='text' placeholder='Recipient Name' />
+                        <input type='text' placeholder='Your Name' />
+                        <input type='text' placeholder='$' />
+                        < input type='text' placeholder='Your message' id='message'/>
 
-                    <button className='btn'>Sign In</button>
-                </form>
+                    </form>
+                    <span>
+                        <button id="btn">Generate</button></span>
+                </div>
+
+                <div className='col-2'>
+                    <img src={bgimg} alt=''></img>
+                </div>
             </div>
-
-            <div className='col-2'>
-                <img src={bgimg}alt=''></img>
-            </div>
-        </div>
-    </section>
-  )
+        </section> 
+    )
 }
 
 export default Form
